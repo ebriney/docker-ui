f961230d433e5939af0793de1884f0cdee5b1162
ebriney <emmanuel.briney@docker.com>
2017-01-04 17:39:12 +0100

    some cleanup
    Signed-off-by: ebriney <emmanuel.briney@docker.com>

--- xcode/webviewtest/webviewtest/ViewController.swift
+++ xcode/webviewtest/webviewtest/ViewController.swift
@@ -4,6 +4,7 @@ import WebKit
 class ViewController: NSViewController, WKScriptMessageHandler {
 
     var webView: WKWebView!
+    let clientExecutable = URL(fileURLWithPath: Bundle.main.path(forResource: "docker-client", ofType: nil)!).absoluteString.replacingOccurrences(of: "file://", with: "")
     
     override func loadView() {
         super.loadView()
@@ -13,11 +14,9 @@ class ViewController: NSViewController, WKScriptMessageHandler {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let url = URL(string: "https://www.google.com")
         let url = URL(fileURLWithPath: Bundle.main.path(forResource: "index", ofType: "html")!)
         let request = URLRequest(url: url)
 
-//        webView.mainFrame.load(request)
         let contentController = WKUserContentController();
         let userScript = WKUserScript(source: "redHeader()", injectionTime: WKUserScriptInjectionTime.atDocumentEnd, forMainFrameOnly: true)
         contentController.addUserScript(userScript)
@@ -29,7 +28,6 @@ class ViewController: NSViewController, WKScriptMessageHandler {
         self.webView = WKWebView(frame: self.view.frame, configuration: config)
         self.view = self.webView!
         webView.load(request)
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
@@ -42,14 +40,13 @@ class ViewController: NSViewController, WKScriptMessageHandler {
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if(message.name == "info") {
             let info = getDockerInfo().replacingOccurrences(of: "\n", with: "")
-//            let js = "dockerInfo = JSON.parse(\'\(info)\');\ndockerInfoChanged();"
             let js = "dockerInfoChanged( JSON.parse(\'\(info)\') );"
             self.webView.evaluateJavaScript(js) { (_, error) in
                 print(error ?? "Info updated successfully")
             }
         } }
 
-    // Execute a shell command and return error code
+    // Execute a shell command and return (error?, stdout)
     func Execute(_ command: String) -> (String?, String) {
         let task = Process()
         task.launchPath = "/bin/sh"
@@ -68,8 +65,7 @@ class ViewController: NSViewController, WKScriptMessageHandler {
     }
     
     func getDockerInfo() -> String {
-        let url = URL(fileURLWithPath: Bundle.main.path(forResource: "docker-client", ofType: nil)!)
-        let (error, output) = Execute(url.absoluteString.replacingOccurrences(of: "file://", with: "") + " -info -compact")
+        let (error, output) = Execute(clientExecutable + " -info -compact")
         if let error = error {
             return error
         }
