046634aa5925b1b988b2d5fdfdf58d560c08c251
ebriney <emmanuel.briney@docker.com>
2017-01-04 11:49:17 +0100

    add host option, dump disk usage and containers

--- go/docker-client/main.go
+++ go/docker-client/main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
+var (
+	host string
+)
+
+func init() {
+	flag.StringVar(&host, "host", client.DefaultDockerHost, "host to reach")
+}
+
 func main() {
-	cli, err := client.NewEnvClient()
+	flag.Parse()
+	ctx := context.Background()
+
+	cli, err := client.NewClient(host, "", nil, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	df, err := cli.DiskUsage(ctx)
+	if err != nil {
+		fmt.Printf("Cannot get disk usage\n")
+	} else {
+		fmt.Printf("Disk usage: %d\n", df.LayersSize)
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
 	if err != nil {
 		panic(err)
 	}
