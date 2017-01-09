3875b49b03fb04fefeb3f2c6d4378d0aacc9009e
Emmanuel Briney <emmanuel.briney@gmail.com>
2017-01-09 17:10:42 +0100

    fix IE7 compat, and parse json
    Signed-off-by: Emmanuel Briney <emmanuel.briney@gmail.com>

--- visual/webviewtestWPF/webviewtestWPF/Resources/main.js
+++ visual/webviewtestWPF/webviewtestWPF/Resources/main.js
@@ -1,3 +1,32 @@
+//Fix for IE7 and lower 
+if (!document.querySelectorAll) {
+    document.querySelectorAll = function (selectors) {
+        var style = document.createElement('style'), elements = [], element;
+        document.documentElement.firstChild.appendChild(style);
+        document._qsa = [];
+
+        style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+        window.scrollBy(0, 0);
+        style.parentNode.removeChild(style);
+
+        while (document._qsa.length) {
+            element = document._qsa.shift();
+            element.style.removeAttribute('x-qsa');
+            elements.push(element);
+        }
+        document._qsa = null;
+        return elements;
+    };
+}
+
+//Fix for IE7 and lower 
+if (!document.querySelector) {
+    document.querySelector = function (selectors) {
+        var elements = document.querySelectorAll(selectors);
+        return (elements.length) ? elements[0] : null;
+    };
+}
+
 function dockerClientRequest(command) {
     try {
         window.external.docker(command);
@@ -10,7 +39,8 @@ function redHeader() {
     document.querySelector('h1').innerHTML = 'Docker is not running';
 }
 
-function docker_info(info) {
+function docker_info(infoJSON) {
+    var info = JSON.parse(infoJSON);
     document.querySelector('h1').innerHTML = 'version: ' + info.ServerVersion;
 }
 
